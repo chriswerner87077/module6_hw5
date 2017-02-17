dea29c396c9ab526a900f2f9759e8da52962520c
Chris Werner <chris.lex.werner@gmail.com>
2017-02-17 02:21:20 +0000

    Initial commit for task2, copying file created elsewhere.

---
+++ gluons_hw5_task2.c
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  chris_werner_hw5_task2.c
+ *       	Usage:  ./chris_werner_hw5_task2.c
+ *    Description:  Write a function to display a table of miles and kilometers
+ *    Stopping and starting values will be used. the function will use these 
+ *    starting and stopping values and increment
+ *
+ *        Version:  1.0
+ *        Created:  02/16/2017 05:07:00 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Chris Werner (), christopherwerner@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+	//Prompt user for the starting miles
+	//Scan input for the starting miles
+	//Prompt user for the ending miles
+	//Scan input for the ending miles
+	//Prompt user for the increment values
+	//Scan input for the increment values
+	//Print the header for the table
+	//Enter loop
+		//Print the mile
+		//Print the kilometer
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
